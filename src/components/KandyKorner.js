7907c0b15edb427284720921c7ec4a9d732c650a
Adam Donaghy <donaghyam@gmail.com>
2022-03-08 11:14:30 -0600

    added authentication

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,17 +1,54 @@
 import React from "react";
-import { Route } from "react-router-dom"
-import { NavBar } from "./nav/NavBar";
+import { Route, Redirect } from "react-router-dom";
 import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+
 
 //The purpose of this component is to determine the general layout of the site 
 //  - determines which order components should be displayed
 
-export const KandyKorner = () => {
-    return (
-        //Fragment allows you to return one JSX element instead of a parent with mulitple child elements.
-        <>
-            <NavBar />
-            <ApplicationViews />
-        </>
-    )
-}
+
+export const KandyKorner = () => (
+
+  //JSX - Language that looks like HTML 
+  //    - React takes JSX, converts its to Javascript, then renders HTML from that
+  //    - use {} for Javascript interpolation 
+
+  //Fragment allows you to return one JSX element instead of a parent with mulitple child elements.
+  <>
+
+    {/* Router handles rendering of different components when the user clicks on navigation items.
+      - this allows the user to bookmark specific places within a single web application */}
+
+    {/* This function determines which component should be rendered based on criteria -
+    It's checking if there is currently something in local storage (meaning someone has logged in)
+    Otherwise, it will be redirected to login */}
+    <Route
+      render={() => {
+        if (localStorage.getItem("kandy_customer")) {
+
+          //In any React component that generates HTML, you always need a return statement with ()
+
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    {/* These routes direct which url the user will be taken to upon clicking the corresponding button */}
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
