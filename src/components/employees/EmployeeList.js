fdb2d37339e7324cd2731a81c91d10a7a7b6f9c2
Adam Donaghy <donaghyam@gmail.com>
2022-03-09 14:30:57 -0600

    fire button added

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -9,21 +9,39 @@ export const EmployeeList = () => {
     const [employees, changeEmployee] = useState([])
     const history = useHistory()
 
+    //Create function to store fetch call for tickets, so it can be called on when the page is rendered,
+    //and when the delete button is clicked.
+    const getEmployees = () => {
+        //Get data from API to pull into application state of tickets
+        fetch("http://localhost:8088/employees?_expand=location")
+            //Convert JSON encoded string into Javascript
+            .then(res => res.json())
+            //Invoke updateTickets() to set value of tickets
+            .then((data) => {
+                changeEmployee(data)
+        })
+    }
+    
     //This hook's main purpose is to observe one, or more, 
     //state variables, and then run code when that state changes.
     useEffect(
         () => {
-            //Get data from API to pull into application state of employees
-            fetch("http://localhost:8088/employees?_expand=location")
-            //Convert JSON encoded string into Javascript   
-            .then(res => res.json())
-            //Invoke changeEmployee() to set value of employees 
-            .then((data) => {
-                changeEmployee(data)
-            })
+            //call getEmployees() to pull employee data into application state
+            getEmployees()
         },
         []
     )
+
+    //Create function to fire an employee, with a parameter of id from the URL
+    const fireEmployee = (id) => {
+        //Get data from API to pull into application state of tickets
+        fetch(`http://localhost:8088/employees/${id}`, {
+            //delete the ticket
+            method: "DELETE"
+        })
+        //then run the getTickets function to update list without the deleted items
+        .then(getEmployees())
+    }
 
     return (
         <>
@@ -42,6 +60,10 @@ export const EmployeeList = () => {
                             Full time: {JSON.stringify(employee.fullTime)}<br></br>
                             Rate: ${employee.hourlyRate}/hr
                             </p>
+                            <button onClick={() => {
+                                fireEmployee(employee.id)
+                                }}>Fire
+                            </button>
                             </div>
                     }
                 )
